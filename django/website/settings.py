482866cb2acf34850ff90e7e1351cb7ff9dcaab3
Hamish Downer <hamish@aptivate.org>
2013-10-14 15:03:46 +0100

    settings can add EXTRA_ bits from local_settings

--- django/website/settings.py
+++ django/website/settings.py
@@ -160,4 +160,34 @@ LOGGING = {
 MONKEY_PATCHES = ['intranet_binder.monkeypatches']
 
 # tasks.py expects to find local_settings.py so the database stuff is there
-from local_settings import *
+#--------------------------------
+# local settings import
+#from http://djangosnippets.org/snippets/1873/
+#--------------------------------
+try:
+    import local_settings
+except ImportError:
+    print """
+    -------------------------------------------------------------------------
+    You need to create a local_settings.py file. Run ../../deploy/tasks.py
+    deploy:<whatever> to use one of the local_settings.py.* files as your
+    local_settings.py, and create the database and tables mentioned in it.
+    -------------------------------------------------------------------------
+    """
+    import sys
+    sys.exit(1)
+else:
+    # Import any symbols that begin with A-Z. Append to lists any symbols that
+    # begin with "EXTRA_".
+    import re
+    for attr in dir(local_settings):
+        match = re.search('^EXTRA_(\w+)', attr)
+        if match:
+            name = match.group(1)
+            value = getattr(local_settings, attr)
+            try:
+                globals()[name] += value
+            except KeyError:
+                globals()[name] = value
+        elif re.search('^[A-Z]', attr):
+            globals()[attr] = getattr(local_settings, attr)
